b6aadab7f1fc66da518fa2bbf38132f256b98089
vrindha19 <146421062+vrindha19@users.noreply.github.com>
2023-12-07 14:48:31 +0530

    Add files via upload

--- project_3/pythonapp/urls.py
+++ project_3/pythonapp/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
-from.views import Registercreate
+from.views import Registercreate,RegisterList,RegisterDetailView,RegisterUpdateView,RegisterDeleteView
 # LoginList,LoginDetailView,LoginUpdateView
 
 
 urlpatterns=[
      path('register/create',Registercreate.as_view() ,name='register_create'),
-    #  path('register/list',RegisterList.as_view(),name='register_list'),
-    #  path('login/detail/<int:pk>/',LoginDetailView.as_view(),name='login_detail'),
-    #  path('login/update/<int:pk>/',LoginUpdateView.as_view(),name='login_update'),
+      path('register/list',RegisterList.as_view(),name='register_list'),
+     path('register/detail/<int:pk>/',RegisterDetailView.as_view(),name='register_detail'),
+     path('register/update/<int:pk>/',RegisterUpdateView.as_view(),name='register_update'),
+    path('register/delete/<int:pk>/',RegisterDeleteView.as_view(),name='register_delete'),
+     
 ]
--- project_3/pythonapp/views.py
+++ project_3/pythonapp/views.py
@@ -5,9 +5,12 @@ from django.shortcuts import render
 
 # # Create your views here.
 from django.views.generic.edit import CreateView
-# from django.views.generic.edit import UpdateView
+from django.views.generic.list import ListView
+from django.views.generic.edit import UpdateView
 # from django.views.generic.list import RegisterView
-# from django.views.generic.detail import DetailView
+from django.views.generic.detail import DetailView
+from django.views.generic import DeleteView
+from django.urls import reverse_lazy
 
 from .models import RegisterModel
 
@@ -17,7 +20,30 @@ class Registercreate(CreateView):
     template_name='registerform_create.html'
     success_url='/success/'
 
-# class RegisterList(RegisterView):
-#       model= RegisterModel
-#       template_name='login_list.html'
-#       context_object_name='login_list'
+class RegisterList(ListView
+                   ):
+      model= RegisterModel
+      template_name='registerform_list.html'
+      context_object_name='register_list'
+
+class RegisterDetailView(DetailView):
+      model= RegisterModel
+      template_name='registerform_detail.html'
+      context_object_name='register_Model'
+
+class RegisterUpdateView(UpdateView):
+    model = RegisterModel  # Specify your model
+    template_name = 'registerform_update.html' 
+    success_url='/success/' # Specify your template name
+    fields = '__all__'  # Use '__all__'     
+
+class RegisterDeleteView(DeleteView) :  
+      model=RegisterModel
+      template_name='registerform_delete.html'
+      success_url = reverse_lazy('register_list')
+
+      def get_context_data(self, **kwargs):
+            context=super().get_context_data(**kwargs)  
+            context['name'] = 'confirm delete login object'
+            return context 
+
